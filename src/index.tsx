4b39400fc93f4fc9802f1c3b306db0f177e30fbd
sarunthinkable <sarthak.singh@unthinkable.co>
2024-11-07 13:50:18 +0530

    added test config

--- src/index.tsx
+++ src/index.tsx
@@ -10,13 +10,11 @@ import { ThemeProvider } from './contexts/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </Provider>
 );
 
 reportWebVitals(console.log);
