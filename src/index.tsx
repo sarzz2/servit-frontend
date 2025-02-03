ec1aa84a3a4d640c96b9dc3c4f9cb229b8883f72
Sarthak singh <sarthaksingh2001@gmail.com>
2025-02-03 11:07:55 +0530

    package update

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './Store';
 import { ThemeProvider } from './contexts/ThemeContext';
@@ -17,4 +17,4 @@ root.render(
   </Provider>
 );
 
-reportWebVitals(console.log);
+// reportWebVitals(console.log);
